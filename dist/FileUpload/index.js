fca92cd79740ab4dbc03683aafc671a3064d1bdd
a-simplecode <amineamine19961996@gmail.com>
2022-05-24 15:58:51 +0300

    FIX: make build before push

--- dist/FileUpload/index.js
+++ dist/FileUpload/index.js
@@ -33,10 +33,6 @@ var _core = _interopRequireDefault(require("@uppy/core"));
 
 var _awsS = _interopRequireDefault(require("@uppy/aws-s3"));
 
-require("@uppy/core/dist/style.css");
-
-require("@uppy/dashboard/dist/style.css");
-
 function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
 
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
